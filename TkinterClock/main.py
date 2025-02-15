d4087d43be2485e2718afee0c02b20ef5cd2284d
CoolCodeMan <81040054+CoolCodeMan@users.noreply.github.com>
2025-02-15 19:58:25 +0200

    Create main.py

---
+++ TkinterClock/main.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from tkinter import ttk
+from time import strftime
+
+def Mainfunction():
+
+    root = Tk()
+    root.attributes("-fullscreen", True)
+
+    background = Frame(root, background='white', width=1920, height=1080)
+    background.place(x = 0, y = 0)
+
+    def time():
+        string = strftime("%H : %M")
+        clockLabel.config(text= string, font=('Gothic', 400), foreground='black', background='white')
+        clockLabel.after(1000, time)
+
+    clockLabel = ttk.Label(root)
+    clockLabel.place(relx=0.5, rely=0.5, anchor=CENTER)
+    time()
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    Mainfunction()
